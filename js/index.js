21c421a7c608ea0dbe1751f3fba1086fb51c6a52
Sameed1990 <111309698+Sameed1990@users.noreply.github.com>
2022-08-31 16:35:31 -0700

    summary again

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// navigation function 
+function OpenModal(){
+    document.getElementById("Mob-Nav").style.display="flex";
+}
+
+function CloseModal(){
+    document.getElementById("Mob-Nav").style.display="none ";
+}
+
+
+// AOS.init();
